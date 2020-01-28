9f40f1e56745f225e7d44fadc533c5562be00be6
Cesar Aaron <aaronash@live.com>
2020-01-28 14:26:22 -0600

    Create a more appealing 404.js page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { makeStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+import { Link } from 'gatsby'
 
-const NotFoundPage = () => (
-  <>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </>
-)
+const NotFoundPage = () => {
+  const styles = useStyles()
+
+  return (
+    <div className={styles.root}>
+      <CssBaseline />
+      <Typography variant="h1">404</Typography>
+      <div>
+        <Typography variant="body2">
+          La pagina que andas buscando no existe,{' '}
+          <Link to="/">regresa a la pagina principal</Link>
+        </Typography>
+      </div>
+    </div>
+  )
+}
 
 export default NotFoundPage
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: '100vh',
+    marginTop: '-50px',
+  },
+}))
